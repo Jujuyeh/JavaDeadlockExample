7969d82122d3a9a52a88951c864a923e21d6632f
Paul Hodgetts <paul.hodgetts@sysdig.com>
2025-03-11 14:29:01 +0100

    Fix Deadlock

--- Deadlock.java
+++ Deadlock.java
@@ -1,37 +1,33 @@
-public class Deadlock { 
-    Integer a = 0;
-    Integer b = 1;
-     
-    Thread th1 = new Thread("Thread 1"){
-        public void run(){
-            while(true){
-                synchronized(a){
-                    synchronized(b){
-                        System.out.printf("%d - %d\n", a, b);
-                    }
-                }
-            }
-        }
-    };
-     
-    Thread th2 = new Thread("Thread 2"){
-        public void run(){
-            while(true){
-                synchronized(b){
-                    synchronized(a){
-                        System.out.printf("%d - %d\n", b, a);
-                    }
-                }
-            }
-        }
-    };
+public class DeadlockFixed { 
+    private final Integer a = 0;
+    private final Integer b = 1;
 
-    Deadlock() {
-	th1.start();
-	th2.start();
-    } 
-     
-    public static void main(String a[]){
-        Deadlock dl = new Deadlock();
+    private final Thread th1 = new Thread(() -> {
+        while (true) {
+            synchronized (a) {
+                synchronized (b) {
+                    System.out.printf("%d - %d\n", a, b);
+                }
+            }
+        }
+    }, "Thread 1");
+
+    private final Thread th2 = new Thread(() -> {
+        while (true) {
+            synchronized (a) { // Locking in the same order as th1
+                synchronized (b) {
+                    System.out.printf("%d - %d\n", a, b);
+                }
+            }
+        }
+    }, "Thread 2");
+
+    public DeadlockFixed() {
+        th1.start();
+        th2.start();
+    }
+
+    public static void main(String[] args) {
+        new DeadlockFixed();
     }
 }
